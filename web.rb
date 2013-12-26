59bb43891258396ca99153a0df3e83625a97b65d
danilat <danilat83@gmail.com>
2013-12-26 19:09:47 +0100

    implemented insert data to cartodb

--- web.rb
+++ web.rb
@@ -5,18 +5,22 @@ require File.join(File.dirname(__FILE__), '', 'config') #to externalize API_KEY 
 
 get '/' do
   sql = "SELECT * from bancosdealimentos WHERE the_geom is not NULL"
-  url = "http://danilat.cartodb.com/api/v2/sql?q=#{sql}&api_key=#{API_KEY}"
-  response = RestClient.get  URI.escape(url)  
+  url = "https://danilat.cartodb.com/api/v2/sql?q=#{sql}&api_key=#{API_KEY}"
+  response = RestClient.get URI.escape(url)  
   data = JSON.parse(response)
-  puts response
   erb :index, :locals => {:data => data}
 end
 
-post 'create' do
-  sql = "INSERT INTO bancosdealimentos (address, contact, type) VALUES ('#{address}', '#{contact}', 'autogestionado')"
-  url = "http://danilat.cartodb.com/api/v2/sql?q=#{sql}&api_key=#{API_KEY}"
-  response = RestClient.post url
-  puts response.code
-  redirect to('/?ok=ok')
+post '/create' do
+  address = "#{params[:address]}. #{params[:place]}"
+  contact = params[:contact]
+  if params[:address] && params[:place] && params[:address] != "" && params[:place] != ""
+    sql = "INSERT INTO bancosdealimentos (full_address, contact, type) VALUES ('#{address}', '#{contact}', 'autogestionado')"
+    url = "https://danilat.cartodb.com/api/v2/sql?api_key=#{API_KEY}"
+    response = RestClient.post URI.escape(url), :q => sql
+    redirect to('/?ok=ok')
+  else
+    redirect to('/?ok=no')
+  end
 end
 
